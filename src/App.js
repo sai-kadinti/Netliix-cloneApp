7e4aed570f3c4e3da192e5f25beaa1b4a422b004
Sai-kumar <saikumarddd@gmail.com>
2025-07-24 01:37:38 +0530

    Uploading all

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-black text-white min-h-screen">
+      <header className="p-4 text-2xl font-bold">Netflix Clone</header>
+      <main className="p-4">
+        <p>Welcome to the Netflix Clone!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
